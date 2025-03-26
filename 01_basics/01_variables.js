0fdfd1f74218d9f0cbae4af80bb0e3387819c8bb
Akash Verma <154662758+code-with-akash@users.noreply.github.com>
2025-03-26 11:37:29 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 112234;
+var accountPassword = "123456";
+accountCity = "noida";
+let accountEmail = "akash@google.com";
+ 
+let accountState;
+accountPassword = "6789";
+accountCity = "bengluru";
+accountEmail = "akash@gmail.com";
+console.table([accountPassword, accountCity, accountEmail, accountState]);
+/* 
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all JS code as newer version
+// alert(3 + 3) we are using node js not browser
+
+/* DataTypes
+number = 2
+string = "Akash"
+boolean = true/false
+bigint 
+null => standalone value
+undefined
+symbol => unique
+
+
+
+object
+
+
+
+
+
+
+*/
+
+console.log(typeof "akash");
+console.log(typeof null); // type of null is object
+console.log(typeof undefined); // type of undefined is undefined
+
+
