4ffce7997855b713e95bb62ac3dffbb9dcb864c2
Akash Verma <154662758+code-with-akash@users.noreply.github.com>
2025-03-26 16:44:44 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = true
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber)
+
+let isLoggedIn = "undefined"
+let booleanIsLoggedIn  = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+let someNumber = 44
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
