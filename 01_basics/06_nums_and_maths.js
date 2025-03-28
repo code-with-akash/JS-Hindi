22c9ec18fd7bd3e929ccc3d3952082537ea8c2fd
Akash Verma <154662758+code-with-akash@users.noreply.github.com>
2025-03-28 03:25:24 +0000

    Number and Math

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,39 @@
+// const score = 400;
+// console.log(score);
+// console.log(typeof score);
+
+
+// const balance = new Number(200);
+// console.log(balance);
+// console.log(typeof balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 2223.8968;
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ----------------- Maths -------------------
+
+// console.log(Math);
+// console.log(Math.abs(-7));
+// console.log(Math.round(33.3));
+// console.log(Math.ceil(33.3));
+// console.log(Math.floor(22.9));
+// console.log(Math.sqrt(25));
+// console.log(Math.min(3,9,5,2,8));
+// console.log(Math.max(3,9,5,2,8));
+
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min)
