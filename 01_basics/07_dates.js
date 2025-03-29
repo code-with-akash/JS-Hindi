767a6eb7e06b0429437fdce7a6f966fdc61dbc9f
Akash Verma <154662758+code-with-akash@users.noreply.github.com>
2025-03-29 05:45:04 +0000

    date in js

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,16 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate, 1);
+// console.log(myDate.toString(), 2);
+// console.log(myDate.toDateString(), 3);
+// console.log(myDate.toISOString(), 4);
+// console.log(myDate.toJSON(), 5);
+// console.log(myDate.toLocaleDateString(), 6);
+// console.log(myDate.toLocaleString(), 7);
+// console.log(myDate.toLocaleTimeString(), 8);
+
+// console.log(typeof myDate)
+
+// let myCreateDate = new Date(2025,02,01);
+// console.log(myCreateDate.toDateString())
